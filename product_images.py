7a32bfc38cc7e05e900b0471699ebc73fdaed27d
lukebranch <lukebranch@gmail.com>
2014-11-11 11:57:19 +0800

    Create product_images.py

---
+++ product_images.py
@@ -0,0 +1,30 @@
+from openerp.osv import osv, fields
+
+class theme_genesis(osv.Model):
+    _inherit = 'product.template'
+    
+    _columns = {
+        'x_product_image_1': fields.binary('Image #1'),
+        'x_product_image_2': fields.binary('Image #2'),
+        'x_product_image_3': fields.binary('Image #3'),
+        'x_product_image_4': fields.binary('Image #4'),
+        'x_product_image_5': fields.binary('Image #5'),
+        'x_product_image_6': fields.binary('Image #6'),
+        'x_product_image_7': fields.binary('Image #7'),
+        'x_product_image_8': fields.binary('Image #8'),
+        'x_product_image_9': fields.binary('Image #9'),
+        'x_product_image_10': fields.binary('Image #10'),
+        'x_product_image_11': fields.binary('Image #11'),
+        'x_product_image_12': fields.binary('Image #12'),
+        'x_product_image_13': fields.binary('Image #13'),
+        'x_product_image_14': fields.binary('Image #14'),
+        'x_product_image_15': fields.binary('Image #15'),
+        'x_product_image_16': fields.binary('Image #16'),
+        'x_product_image_17': fields.binary('Image #17'),
+        'x_product_image_18': fields.binary('Image #18'),
+        'x_product_image_19': fields.binary('Image #19'),
+        'x_product_image_20': fields.binary('Image #20'),
+        'x_product_image_21': fields.binary('Image #21'),
+        'x_product_image_22': fields.binary('Image #22'),
+        
+    }
