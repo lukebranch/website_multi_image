df9084f87c6e793f15ecde2a586ee4415a7fa985
lukebranch <lukebranch@gmail.com>
2014-11-11 11:56:49 +0800

    Create __openerp__.py

---
+++ __openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Genesis Product Multi-Image',
+    'description': 'This module adds multiple product images (22 currently) into a tab in product.template in a tab called Product Images',
+    'category': 'Website',
+    'version': '1.0',
+    'author': 'Luke Branch',
+    'depends': ['website', 'website_sale'],
+    'data': [
+        'views/product_images.xml', 
+    ],
+    'application': True,
+}
