c868acafb77a94fe7717a2f719011f1a104d00fc
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2014-11-26 22:04:06 -0300

    [FIX] File

--- __openerp__.py
+++ __openerp__.py
@@ -1,12 +1,13 @@
 {
-    'name': 'Genesis Product Multi-Image',
+    'name': 'Product Multi-Image',
     'description': 'This module adds multiple product images (22 currently) into a tab in product.template in a tab called Product Images',
     'category': 'Website',
     'version': '1.0',
     'author': 'Luke Branch',
-    'depends': ['website', 'website_sale'],
+    'depends': ['product','sale'],
     'data': [
         'views/product_images.xml', 
+        'security/ir.model.access.csv', 
     ],
     'application': True,
 }
