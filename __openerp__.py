588baf824cb2b2d459e81ac23cdb11004ff5e645
Luke Branch <lukebranch@gmail.com>
2014-11-29 18:23:20 +0800

    Update __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -3,7 +3,7 @@
     'description': 'This module adds multiple product images (22 currently) into a tab in product.template in a tab called Product Images',
     'category': 'Website',
     'version': '1.0',
-    'author': 'A Collaboration between Luke Branch and Cristian Sebastian Rocha',
+    'author': 'Luke Branch and Cristian Sebastian Rocha',
     'depends': ['product','sale'],
     'data': [
         'views/product_images.xml', 
