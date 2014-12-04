14735110d05e293f180891f235bea8ba3043a95c
Luke Branch <lukebranch@gmail.com>
2014-12-04 13:27:25 +0800

    Update __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Product Multi-Image',
-    'description': 'This module adds multiple product images (22 currently) into a tab in product.template in a tab called Product Images',
+    'description': 'This module adds multiple product images into a tab in product.template in a tab called Product Images, and allows an over-ride of product.image to allow for a multi-image carousel for the product view page.',
     'category': 'Website',
     'version': '1.0',
     'author': 'Luke Branch and Cristian Sebastian Rocha',
