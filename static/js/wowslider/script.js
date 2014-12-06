47b9acd5e13767c18c003e5e675e4104bd062b56
Luke Branch <lukebranch@gmail.com>
2014-12-06 17:50:47 +0800

    Delete script.js

--- static/js/wowslider/script.js
+++
@@ -1,1 +0,0 @@
-jQuery("#wowslider-container").wowSlider({effect:"rotate",prev:"",next:"",duration:20*100,delay:20*100,width:580,height:212,autoPlay:true,stopOnHover:false,loop:false,bullets:true,caption:true,captionEffect:"slide",controls:true,logo:"",images:0});
